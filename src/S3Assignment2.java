6febad2b1001a32864a62d2c068fa774a379efcd
analysedata <acadgild@mawapps.com>
2016-11-29 14:30:49 -0800

    A class that can be used to compare 2 strings
    The TwoStrings method uses the equals() method to compare 2 strings.

--- src/S3Assignment2.java
+++ src/S3Assignment2.java
@@ -3,6 +3,8 @@ public class S3Assignment2 {
 	public static void main(String[] args) {
 		
 		// Strings to compare
+		String x = "String 1";
+		String y = "String 2";
 		
 		// Instantiate a new object of type TwoStrings
 		TwoStrings twoStringsObject = new TwoStrings(x, y);
@@ -20,6 +22,7 @@ class TwoStrings {
 	String s1;
 	String s2;
 	
+	// Constructor with 2 string parameters
 	TwoStrings(String str1, String str2) {
 		s1 = str1;
 		s2 = str2;
@@ -30,4 +33,13 @@ class TwoStrings {
 
 		boolean same = false;
 
+		// Use the equals() method to compare s1 with s2
+		if (s1.equals(s2)) {
+			same = true;
+		} else {
+			same = false;
+		}
+
+		return same;
+	}
 }
